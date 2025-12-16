31a3883e66c8d959f6cfe062ffce55b39d42f294
azizadipramana <aziz.adipramana24@gmail.com>
2025-12-16 23:17:18 +0700

    Change hint display color and decrement hint used
    Updated hint display color and adjusted hint count.

--- puzzle_final.py
+++ puzzle_final.py
@@ -496,8 +496,8 @@ def draw_game_interface():
 
         # TAMPILAN SCORE
 
-        hint_out = font.render(f"HINT USED: {hint_used}", True, "red")
-        screen.blit(hint_out, (400, 400))
+        hint_out = font.render(f"HINT USED: {hint_used - 1}", True, "white")
+        screen.blit(hint_out, (395, 400))
         
         score_text = font.render(f"SCORE: {player_score}", True, "white")
         screen.blit(score_text, (400, 350))
